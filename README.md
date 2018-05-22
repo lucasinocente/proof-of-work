4b563708064d2bce62e74113def338ab875d08eb
Lucas Inocente <olucassanchez@gmail.com>
2018-05-22 01:12:04 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proof Of Work Example
+
+## To run
+
+`node index.js <infotobecrypto>`
+
+--
+
+Inspired by https://gist.github.com/othiym23/5639370
---
+++ index.js
@@ -0,0 +1,31 @@
+const createHash = require('crypto').createHash;
+
+function work(input) {
+    
+    let id = 0;
+
+    console.time('Time to find');
+
+    while (true) {
+
+        let nonce = id.toString();
+        let sha256 = createHash('sha256');
+
+        sha256.update(input);
+        sha256.update(nonce);
+
+        let hash = sha256.digest('hex');
+
+        console.log(`Nonce: ${nonce} -  Hash: ${hash}`);
+
+        if (hash.slice(-5) === '00000') {
+            console.timeEnd('Time to find');
+            return;
+        } else {
+            id++;
+        }
+
+    }
+}
+
+work(process.argv[2]);
